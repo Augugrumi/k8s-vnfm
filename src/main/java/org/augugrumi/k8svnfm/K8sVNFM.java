993c873525d4fa4dac9858b7154aad1b90f98841
mzanella <zanna0150@gmail.com>
2018-08-19 17:53:07 +0200

    Initial commit

---
+++ src/main/java/org/augugrumi/k8svnfm/K8sVNFM.java
@@ -0,0 +1,164 @@
+package org.augugrumi.k8svnfm;
+
+import org.openbaton.catalogue.mano.descriptor.VNFComponent;
+import org.openbaton.catalogue.mano.record.VNFCInstance;
+import org.openbaton.catalogue.mano.record.VNFRecordDependency;
+import org.openbaton.catalogue.mano.record.VirtualNetworkFunctionRecord;
+import org.openbaton.catalogue.nfvo.Action;
+import org.openbaton.catalogue.nfvo.Script;
+import org.openbaton.catalogue.nfvo.viminstances.BaseVimInstance;
+import org.openbaton.common.vnfm_sdk.amqp.AbstractVnfmSpringAmqp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+import java.util.Map;
+
+@SpringBootApplication
+public class K8sVNFM extends AbstractVnfmSpringAmqp {
+    public static void main(String[] args) {
+        SpringApplication.run(K8sVNFM.class);
+    }
+
+    /**
+     * This operation allows creating a VNF instance.
+     *
+     */
+    @Override
+    public VirtualNetworkFunctionRecord instantiate(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, Object o, Map<String, Collection<BaseVimInstance>> map) throws Exception {
+        return null;
+    }
+
+    /**
+     * This operation allows retrieving
+     * VNF instance state and attributes.
+     */
+    @Override
+    public void query() {
+
+    }
+
+    /**
+     * This operation allows scaling
+     * (out/in, up/down) a VNF instance.
+     */
+    @Override
+    public VirtualNetworkFunctionRecord scale(Action scaleInOrOut, VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFComponent component, Object scripts, VNFRecordDependency dependency) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+
+    /**
+     * This operation allows verifying if
+     * the VNF instantiation is possible.
+     */
+    @Override
+    public void checkInstantiationFeasibility() {
+
+    }
+
+    /**
+     * This operation is called when one the VNFs fails
+     */
+    @Override
+    public VirtualNetworkFunctionRecord heal(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFCInstance component, String cause) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    /**
+     * This operation allows applying a minor/limited
+     * software update (e.g. patch) to a VNF instance.
+     */
+    @Override
+    public VirtualNetworkFunctionRecord updateSoftware(Script script, VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+
+    /**
+     * This operation allows making structural changes
+     * (e.g. configuration, topology, behavior,
+     * redundancy model) to a VNF instance.
+     *
+     * @param virtualNetworkFunctionRecord
+     * @param dependency
+     */
+    @Override
+    public VirtualNetworkFunctionRecord modify(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFRecordDependency dependency) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    /**
+     * This operation allows deploying a new
+     * software release to a VNF instance.
+     */
+    @Override
+    public void upgradeSoftware() {
+
+    }
+
+    /**
+     * This operation allows terminating gracefully
+     * or forcefully a previously created VNF instance.
+     *
+     * @param virtualNetworkFunctionRecord
+     */
+    @Override
+    public VirtualNetworkFunctionRecord terminate(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) {
+        log.info("Terminating vnfr with id " + virtualNetworkFunctionRecord.getId());
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public void handleError(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) {
+
+    }
+
+    protected void checkEMS(String hostname) {
+        log.warn("EMS is not supported by this VNFM");
+    }
+
+    protected void checkEmsStarted(String hostname) throws RuntimeException {
+        log.warn("EMS is not supported by this VNFM");
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord start(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord stop(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord startVNFCInstance(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFCInstance vnfcInstance) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord stopVNFCInstance(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFCInstance vnfcInstance) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord configure(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    @Override
+    public VirtualNetworkFunctionRecord resume(VirtualNetworkFunctionRecord virtualNetworkFunctionRecord, VNFCInstance vnfcInstance, VNFRecordDependency dependency) throws Exception {
+        return virtualNetworkFunctionRecord;
+    }
+
+    /**
+     * This operation allows providing notifications on state changes
+     * of a VNF instance, related to the VNF Lifecycle.
+     */
+    @Override
+    public void NotifyChange() {
+
+    }
+
+}
